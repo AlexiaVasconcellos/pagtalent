8ab7143a59a9f6de844c6becb605ceb30a1b2cd4
AlexiaVasconcellos <68077007+AlexiaVasconcellos@users.noreply.github.com>
2020-07-17 10:06:48 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,33 @@
+Public String diasemana(int Dia){
+If((dia<0) || (dia>6)){
+Return "erro";
+
+Switch(dia){
+ case 0:
+Return " Domingo";
+Break;
+case 1:
+Return "segunda";
+Break;
+
+case 2:
+Return "Terça";
+Break;
+
+case 3:
+Return "Quarta";
+Break;
+
+case 4:
+Return "Quinta";
+Break;
+
+case 5:
+Return "sexta";
+Break;
+
+case 6:
+Return "sabado";
+Break;
+
+}
